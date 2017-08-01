966e3e16cc76903ea6ff9b452d003846cdd8a717
mkhuda <mhmmd.huda@gmail.com>
2017-08-01 11:46:50 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+console.ignoredYellowBox = ['Setting a timer'];
+AppRegistry.registerComponent('react-native-cyclone', () => App);
