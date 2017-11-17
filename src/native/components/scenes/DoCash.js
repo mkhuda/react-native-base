78ce38ef62ff23b5e0c49b00f71d1add6768d520
mkhuda <mhmmd.huda@gmail.com>
2017-11-17 13:22:15 +0700

    DoCash scene

---
+++ src/native/components/scenes/DoCash.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Container, Content, Header, Title, Body, Left, Right, Icon, Form, Item, Label, Input, Text, Button } from 'native-base';
+import {StyleSheet, View, Image, TouchableOpacity, Alert} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import Loading from '../Loading';
+import Messages from '../Messages';
+import Spacer from '../Spacer';
+import logoImg from '../../../images/logo.png';
+import { logout } from '../../../actions/user';
+
+class DoCash extends Component {
+  static propTypes = {
+    member: PropTypes.shape({
+      email: PropTypes.string,
+    }),
+    logout: PropTypes.func,
+  }
+
+  static defaultProps = {
+    error: null,
+    member: {},
+  }
+  constructor(props) {
+    super(props);
+
+    this._handleLogout = this._handleLogout.bind(this);
+  }
+
+  _handleLogout = () => {
+    this.props.logout();
+  }
+
+  render() {
+
+    return (
+      <Container>
+        <Header>
+            <Left>
+              <Button transparent onPress={() => Actions.pop()}>
+                <Icon name='ios-arrow-back' />
+              </Button>
+            </Left>
+            <Body>
+              <Title>DO Cash</Title>
+            </Body>
+            <Right />
+        </Header>
+        <Content padder>
+        <View style={styles.container}>
+          <View style={styles.containerhome}>
+            <Text>DO Cash</Text>
+          </View>
+        </View>
+        <Spacer size={20} />
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  backgroundtomato: {
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerhome: {
+    width: '100%',
+  },
+});
+
+const mapStateToProps = state => ({
+  member: state.member || {},
+});
+
+const mapDispatchToProps = {
+  logout
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DoCash);
