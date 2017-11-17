9ab14f9e2c6fb88f3265345415ad5fa9ab1917f0
mkhuda <mhmmd.huda@gmail.com>
2017-11-17 13:24:12 +0700

    styling scenes

---
+++ src/native/components/scenes/Style.js
@@ -0,0 +1,52 @@
+export default {
+  backgroundtomato: {
+    backgroundColor: 'transparent',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+  },
+  containerhome: {
+    marginTop: 30,
+    width: '100%',
+  },
+  image: {
+    width: 180,
+    height: 50,
+  },
+  iconhome: {
+    fontSize: 50,
+    color: '#222',
+  },
+  outercolumn: {
+    flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100%',
+  },
+  innerrow: {
+    height: 140, width: '100%',
+  },
+  innercolumn: {
+    flex: 1, alignItems: 'center', justifyContent: 'center',
+  },
+  rowspace: {
+    flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: 'row',
+  },
+  touchable: {
+    borderWidth:1,
+    borderColor:'rgba(0,0,0,0.1)',
+    alignItems:'center',
+    justifyContent:'center',
+    width:100,
+    height:100,
+    backgroundColor:'#fff',
+    borderRadius:100,
+  },
+  rowcard: {
+    width: 120, justifyContent: 'center', alignItems: 'center',
+  },
+  logoutfont: {
+    color: '#222', fontWeight: "normal",
+  }
+};
+
