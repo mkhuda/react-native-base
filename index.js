747cd815ea22b78ce02e9728025ed24d8e209cfe
mkhuda <mhmmd.huda@gmail.com>
2018-05-05 03:38:45 +0700

    rename AppRegistry

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 console.ignoredYellowBox = ['Setting a timer'];
-AppRegistry.registerComponent('react-native-cyclone', () => App);
+AppRegistry.registerComponent('reactnativebase', () => App);
