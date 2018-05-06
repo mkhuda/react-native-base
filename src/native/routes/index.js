51933d95e4db8fc6677c810a85963c7838d318dc
mkhuda <mhmmd.huda@gmail.com>
2018-05-06 10:21:13 +0700

    unused import

--- src/native/routes/index.js
+++ src/native/routes/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { ActionConst, Drawer, Scene, Tabs, Stack } from 'react-native-router-flux';
-import { Icon } from 'native-base';
+import { ActionConst, Stack, Scene } from 'react-native-router-flux';
 
 import DefaultProps from '../constants/navigation';
-import AppConfig from '../../constants/config';
 
 import AuthComponent from '../components/Auth';
 import AuthContainer from '../../containers/Auth';
@@ -16,68 +14,69 @@ import PayplanDoneComponent from '../components/scenes/PayplanDone';
 import DoUnpaidComponent from '../components/scenes/DoUnpaid';
 import DoCashComponent from '../components/scenes/DoCash';
 
+const TRUE = true;
 const Index = (
-    <Stack key="mainstack">
-      <Scene
-          hideNavBar
-          key="auth"
-          {...DefaultProps.navbarProps}
-          component={AuthContainer}
-          Layout={AuthComponent}
-          initial={true}
-          type={ActionConst.REPLACE}
-        />
+  <Stack key="mainstack">
+    <Scene
+      hideNavBar
+      key="auth"
+      {...DefaultProps.navbarProps}
+      component={AuthContainer}
+      Layout={AuthComponent}
+      initial={TRUE}
+      type={ActionConst.REPLACE}
+    />
 
-        {
-          // Main Scenes
-        }
-        <Scene
-          hideNavBar
-          key="home"
-          {...DefaultProps.navbarProps}
-          component={HomeComponent}
-          type={ActionConst.REPLACE}
-        />
-        <Scene
-          hideNavBar
-          key="customer"
-          {...DefaultProps.navbarProps}
-          component={CustomerComponent}
-        />
-        <Scene
-          hideNavBar
-          key="payplanprocess"
-          {...DefaultProps.navbarProps}
-          component={PayplanProcessComponent}
-        />
-        <Scene
-          hideNavBar
-          key="dovalid"
-          {...DefaultProps.navbarProps}
-          component={DoValidComponent}
-        />
-        <Scene
-          hideNavBar
-          key="dounpaid"
-          {...DefaultProps.navbarProps}
-          component={DoUnpaidComponent}
-        />
-        <Scene
-          hideNavBar
-          key="payplandone"
-          {...DefaultProps.navbarProps}
-          component={PayplanDoneComponent}
-        />
-        <Scene
-          hideNavBar
-          key="docash"
-          {...DefaultProps.navbarProps}
-          component={DoCashComponent}
-        />
-        {
-          // End Scenes importir
-        }
-    </Stack>
+    {
+      // Main Scenes
+    }
+    <Scene
+      hideNavBar
+      key="home"
+      {...DefaultProps.navbarProps}
+      component={HomeComponent}
+      type={ActionConst.REPLACE}
+    />
+    <Scene
+      hideNavBar
+      key="customer"
+      {...DefaultProps.navbarProps}
+      component={CustomerComponent}
+    />
+    <Scene
+      hideNavBar
+      key="payplanprocess"
+      {...DefaultProps.navbarProps}
+      component={PayplanProcessComponent}
+    />
+    <Scene
+      hideNavBar
+      key="dovalid"
+      {...DefaultProps.navbarProps}
+      component={DoValidComponent}
+    />
+    <Scene
+      hideNavBar
+      key="dounpaid"
+      {...DefaultProps.navbarProps}
+      component={DoUnpaidComponent}
+    />
+    <Scene
+      hideNavBar
+      key="payplandone"
+      {...DefaultProps.navbarProps}
+      component={PayplanDoneComponent}
+    />
+    <Scene
+      hideNavBar
+      key="docash"
+      {...DefaultProps.navbarProps}
+      component={DoCashComponent}
+    />
+    {
+      // End Scenes importir
+    }
+  </Stack>
 );
 
 export default Index;
