05967f2daa02fa2f5943d3afb347ff83546abf43
mkhuda <mhmmd.huda@gmail.com>
2018-05-06 10:29:42 +0700

    Use HomeComponents only

--- src/native/components/scenes/Home.js
+++ src/native/components/scenes/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Container, Content, Icon, Form, Item, Label, Input, Text, Button, Footer, FooterTab } from 'native-base';
-import {StyleSheet, View, Image, TouchableOpacity, Alert, BackHandler, ToastAndroid, StatusBar} from 'react-native';
+import { Container, Content, Icon, Text, Button, Footer } from 'native-base';
+import { View, Image, TouchableOpacity, Alert, StatusBar } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { logout } from '../../../actions/user';
 import styles from './Style';
@@ -26,41 +26,20 @@ class Home extends React.Component {
   }
 
   componentDidMount = () => {
-    console.log(this.props);
     StatusBar.setHidden(true);
   }
 
   _handleGo = (c) => {
     const handle = c;
     switch (handle) {
-     case 'customer': {
-       Actions.customer();
-       break;
-     }
-     case 'paypro': {
-       Actions.payplanprocess();
-       break;
-     }
-     case 'dova': {
-       Actions.dovalid();
-       break;
-     }
-     case 'paydon': {
-       Actions.payplandone();
-       break
-     }
-     case 'doun': {
-       Actions.dounpaid();
-       break;
-     }
-     case 'doca': {
-       Actions.docash();
-       break;
-     }
-     default: {
-       Alert.alert('Error', 'Something happend');
-        break;
-     }
+    case 'customer': {
+      Actions.customer();
+      break;
+    }
+    default: {
+      Alert.alert('Go to', c);
+      break;
+    }
     }
   }
 
@@ -69,7 +48,6 @@ class Home extends React.Component {
   }
 
   render() {
-    const {email} = this.props.member
     return (
       <Container style={styles.backgroundtomato}>
         <Content padder>
@@ -88,12 +66,6 @@ class Home extends React.Component {
                         </TouchableOpacity>
                         <Text>Customer</Text>
                       </View>
-                      <View style={styles.rowcard}>
-                        <TouchableOpacity style={styles.touchable} onPress={() => this._handleGo('paypro')}>
-                          <Icon name='ios-paper-plane' style={styles.iconhome} />
-                        </TouchableOpacity>
-                        <Text>Payplan Process</Text>
-                      </View>
                     </View>
 
                 </View>
@@ -102,12 +74,6 @@ class Home extends React.Component {
                 <View style={styles.innercolumn}>
 
                     <View style={styles.rowspace}>
-                      <View style={styles.rowcard}>
-                      <TouchableOpacity style={styles.touchable} onPress={() => this._handleGo('dova')}>
-                          <Icon name='ios-checkmark-circle' style={styles.iconhome} />
-                        </TouchableOpacity>
-                        <Text>DO Valid</Text>
-                      </View>
                       <View style={styles.rowcard}>
                         <TouchableOpacity style={styles.touchable} onPress={() => this._handleGo('paydon')}>
                           <Icon name='ios-checkmark-circle' style={styles.iconhome} />
@@ -123,12 +89,6 @@ class Home extends React.Component {
                 <View style={styles.innercolumn}>
 
                     <View style={styles.rowspace}>
-                      <View style={styles.rowcard}>
-                        <TouchableOpacity style={styles.touchable} onPress={() => this._handleGo('doun')}>
-                          <Icon name='ios-cash' style={styles.iconhome} />
-                        </TouchableOpacity>
-                        <Text>DO Unpaid</Text>
-                      </View>
                       <View style={styles.rowcard}>
                         <TouchableOpacity style={styles.touchable} onPress={() => this._handleGo('doca')}>
                           <Icon name='ios-cash' style={styles.iconhome} />
@@ -146,7 +106,7 @@ class Home extends React.Component {
         </Content>
         <Footer style={styles.backgroundtomato}>
           <Button iconLeft transparent block onPress={this._handleLogout}>
-            <Icon name='ios-person-outline' style={styles.logoutfont} />
+            <Icon name="ios-person-outline" style={styles.logoutfont} />
             <Text style={styles.logoutfont}>Logout</Text>
           </Button>
         </Footer>
