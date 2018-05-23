34ae0844acbd1fd31ade0557b6ad67a0a9ad1ef4
mkhuda <mhmmd.huda@gmail.com>
2018-05-24 02:45:14 +0700

    remove unused do_valid store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import status from './status';
 import member from './member';
-import do_valid from './do_valid';
 import { StatusBar, Platform } from 'react-native';
 
 const rehydrated = (state = false, action) => {
