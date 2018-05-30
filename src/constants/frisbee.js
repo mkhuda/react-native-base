0d66e6404f4328cbf3c682a3f7ffde5cb4bce9f8
mkhuda <mhmmd.huda@gmail.com>
2018-05-31 02:35:13 +0700

    frisbee header x-apikey removed

--- src/constants/frisbee.js
+++ src/constants/frisbee.js
@@ -3,7 +3,6 @@ import Frisbee from 'frisbee';
 let api = new Frisbee({
   baseURI: 'https://localhost:3000',
   headers: {
-    'x-apikey': '',
     'Content-Type': 'application/json',
     'Cache-Control': 'no-cache',
   }
